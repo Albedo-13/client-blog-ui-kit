a5687dd8172c01b27cbf3bc9842ab32571364dfd
Pavel Prokopenya <prokopenya.personal@gmail.com>
2024-06-05 19:53:36 +0300

    refactor: remove useless components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { ButtonApp } from "./ui-components/button"
 export { Button } from "./ui-components/buttons"
 export { CheckboxButton } from "./ui-components/checkboxes"
 export { RadioButton } from "./ui-components/radios"
